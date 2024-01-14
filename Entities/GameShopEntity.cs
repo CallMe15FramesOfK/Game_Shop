567c52f471390ffc9af2b5cd995027462dcc0b66
CallMe15FramesOfK <janekkrzeczo@gmail.com>
2024-01-14 15:51:20 +0100

    Dodano klucz główny i css

--- Entities/GameShopEntity.cs
+++ Entities/GameShopEntity.cs
@@ -1,7 +1,10 @@
-﻿namespace Game_Shop.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Game_Shop.Entities
 {
     public class GameShopEntity
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
 
